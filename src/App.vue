1ed88a7ac15d9090f90628c240953489c2ca4f9a
Vladimir Riha <vladimir.riha@memsource.com>
2021-10-11 09:44:24 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <MyButton />
+    Reactive number: {{ randomNumber }}
+  </div>
+</template>
+
+<script lang="ts">
+import MyButton from "ladariha-button/src/components/MyButton.vue";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    MyButton,
+  },
+  setup() {
+    const randomNumber = ref(Math.random());
+
+    setInterval(() => {
+      randomNumber.value = Math.random();
+    }, 3000);
+
+    return { randomNumber };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
