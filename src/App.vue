a189b4910c3f44f688389ee0543d7767c0118e72
Vladimir Riha <vladimir.riha@memsource.com>
2021-10-11 09:59:54 +0200

    Add button v2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
+    <h1>Button from Vue3</h1>
     <MyButton />
+    <h1>Button from Vue2</h1>
+    <MyButton2 />
     Reactive number: {{ randomNumber }}
   </div>
 </template>
 
 <script lang="ts">
 import MyButton from "ladariha-button/src/components/MyButton.vue";
+import MyButton2 from "ladariha-button-2/src/components/MyButton.vue";
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "App",
   components: {
     MyButton,
+    MyButton2,
   },
   setup() {
     const randomNumber = ref(Math.random());
